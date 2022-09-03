cf57c2f1f1f6229db7d01c184be47defd3d25057
charlie1099 <cdcharlesworth@gmail.com>
2022-09-03 14:17:29 -0400

    added gas-report

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,12 +2,14 @@ require("@nomicfoundation/hardhat-toolbox");
 require ("dotenv").config();
 require("@nomiclabs/hardhat-etherscan")
 require("./tasks/block-number")
+require("hardhat-gas-reporter")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -28,6 +30,14 @@ module.exports = {
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
   },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    // token: "MATIC"
+  }
   
 };
 module.exports
